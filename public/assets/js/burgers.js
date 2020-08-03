cdc79839011fb99a437f5fafa574bc399066216d
Hisham Amery <hishamamery@gmail.com>
2020-08-02 21:12:45 -0600

    Delete button working

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -2,7 +2,7 @@
 $(function () {
   $(".change-devoured").on("click", function (event) {
     var id = $(this).data("id");
-    // var newDevoured = $(this).data("newdevoured"); delete later
+    
 
     var newDevouredState = {
       devoured: 1
