499530669cb18712c9959ab5b32611393d108965
Hisham Amery <hishamamery@gmail.com>
2020-08-02 20:53:19 -0600

    Most functionality working, still need delete button

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name) VALUES ('Chicken');
+INSERT INTO burgers (burger_name) VALUES ('Turkey');
+INSERT INTO burgers (burger_name) VALUES ('Cheeseburger');
+
+Select * from burgers;
